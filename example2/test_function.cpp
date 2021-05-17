72a5e488c5870fa7a165618c8403bd850cc3de57
yhou46 <yunpenghou@Yunpengs-MacBook-Pro.local>
2021-05-16 18:44:53 -0700

    use C++ 20 in example2

--- example2/test_function.cpp
+++ example2/test_function.cpp
@@ -1,8 +1,10 @@
 
 #include "test_function.h"
 #include <iostream>
+#include <memory>
 
 void print()
 {
 	std::cout << "test print function\n";
+	std::unique_ptr<int> numPtr = std::make_unique<int>(10);
 }
