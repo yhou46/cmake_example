ee423a5c4e06ecaf537093ab8a970d4f5f92df12
yhou46 <yunpenghou@Yunpengs-MacBook-Pro.local>
2021-05-16 18:37:27 -0700

    use C++ 20 in example1

--- example1/src/example1_main.cpp
+++ example1/src/example1_main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include "test_function.h"
+#include <memory>
 
 int main(int argc, const char* argv[])
 {
 	std::cout << "Hello world\n";
 	print();
+	std::unique_ptr<int> numPtr = std::make_unique<int>(10);
 }
